d0f87504b1ff28fea8874df5fafb30c9b48f131f
Fabricio Magno <fabriciomagnos@gmail.com>
2022-07-18 16:54:58 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
     API_URL: 'http://localhost:5000',
-    MONGO_CONNECTION: 'mongodb+srv://admin:vWhAUnLjQm773rNW@dio-url-shortener.ytdbg.mongodb.net/?retryWrites=true&w=majority',
+    MONGO_CONNECTION: 'mongodb+srv://<user>:<password>@dio-url-shortener.ytdbg.mongodb.net/?retryWrites=true&w=majority',
 
 }
